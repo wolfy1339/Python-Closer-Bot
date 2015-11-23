261c1dc71e56351b659b6ae2763113cd64c228db
wolfy1339 <wolfy1339@users.noreply.github.com>
2015-11-22 22:38:01 -0500

    Initial commit

---
+++ tpt.py
@@ -0,0 +1,204 @@
+###
+# Copyright (c) 2015, wolfy1339
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+#   * Redistributions of source code must retain the above copyright notice,
+#     this list of conditions, and the following disclaimer.
+#   * Redistributions in binary form must reproduce the above copyright notice,
+#     this list of conditions, and the following disclaimer in the
+#     documentation and/or other materials provided with the distribution.
+#   * Neither the name of the author of this software nor the name of
+#     contributors to this software may be used to endorse or promote products
+#     derived from this software without specific prior written consent.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+###
+
+# Notice:
+#   * This requires requests & BeutifulSoup4 from pypi
+#   * This code is fully PEP8 compliant,
+#     and is Python 2 & 3 compatible (should be most of the time)
+# Please respect point 2 of this notice when contributing.
+
+from bs4 import BeautifulSoup
+from config import tpt
+from datetime import datetime
+import os
+import pickle
+import requests
+import requests.utils
+
+
+class TPT(object):
+    # Variables used in the source
+    lockMsg = ''.join(tpt['lockmsg'])
+    key = tpt['key']
+    referer = 'http://powdertoy.co.uk/Groups/Thread/View.html'
+    referer += '?Group=832'
+    whitelist = tpt['whitelist']
+    session = requests.Session()
+    if not os.path.isfile('cookies'):
+        data = {
+            'name': tpt['username'],
+            'pass': tpt['password'],
+            'Remember': 'Yes'
+        }
+        response = session.post(
+            'https://powdertoy.co.uk/Login.html', data=data)
+        with open('cookies', 'w+') as f:
+            pickle.dump(
+                requests.utils.dict_from_cookiejar(session.cookies), f)
+    else:
+        with open('cookies') as f:
+            cookies = requests.utils.cookiejar_from_dict(pickle.load(f))
+            session.cookies.set(cookies.keys()[0], cookies.values()[0])
+
+    def whitelist(self, threadNum):
+        for a in self.whitelist:
+            if threadNum == a:
+                return True
+        return False
+
+    def postRequest(self, url, headers, data, params=None):
+        if params:
+            return self.session.request(
+                'POST', url, headers=headers, data=data, allow_redirects=True,
+                params=params).raise_for_status()
+        else:
+            return self.session.request(
+                'POST', url, headers=headers, data=data,
+                allow_redirects=True).raise_for_status()
+
+    def threadModeration(self, type, threadNum, modKey):
+        # Example headers (includes server response headers):
+        # http://hastebin.com/isugujeyeg.txt
+        referer = self.referer
+        referer += '&Thread={0}'.format(threadNum)
+        headers = {
+            'Referer': referer
+        }
+        if type.lower() == 'lock':
+            data = {
+                'Moderation_Lock': 'Lock'
+            }
+        elif type.lower() == 'delete':
+            data = {
+                'Moderation_Delete': 'true',
+                'Moderation_DeleteConfirm': 'Delete Thread'
+            }
+        moderationURL = 'http://powdertoy.co.uk/Groups/Thread/Moderation.html'
+        params = {
+            'Group': '832',
+            'Thread': threadNum,
+            'Key': modKey
+        }
+        self.postRequest(moderationURL, headers=headers, data=data,
+                         params=params)
+
+    def threadPost(self, message, threadNum, key):
+        # Example headers (includes server response headers):
+        # http://hastebin.com/epidazekah.txt
+        referer = self.referer + '&Thread={0}'.format(threadNum)
+        headers = {
+            'Referer': referer
+        }
+        data = {
+            'Thread': threadNum,
+            'Post_Post': 'Post',
+            'Post_Message': message
+        }
+        threadPostURL = 'http://powdertoy.co.uk/Groups/Thread/Reply.html'
+        params = {
+            'Group': '832',
+            'Key': key
+        }
+        self.postRequest(threadPostURL, headers=headers, data=data,
+                         params=params)
+
+    def timestr_to_obj(datetime_str):
+        # Current date and time in UTC
+        datetime_now = datetime.utcnow()
+        # Current date in "Day Month" format
+        current_date = datetime_now.strftime("%d %B")
+        # Current year
+        current_year = datetime_now.strftime("%Y")
+
+        # Check if the datetime string is in the format "Hour:Minute:Year"
+        if ":" in datetime_str:
+            # Convert it to "Hour:Minute:year Day Month Year"
+            datetime_str = "{0} {1} {2}".format(
+                datetime_str, current_date, current_year)
+
+        # Check if the datetime string is in the format
+        # "Day(st|nd|rd|th) Month"
+        elif datetime_str[0].isdigit():
+            # Remove the ordinal indicator
+            datetime_str = re.sub(r"^(\d+)(st|nd|rd|th)", r"\1", datetime_str)
+            # The day must be a zero-padded decimal number
+            if datetime_str[1] == " ":
+                datetime_str = "0" + datetime_str
+                # Convert it to "Hour:Minute:year Day Month Year"
+                datetime_str = "00:00:00 {0} {1}".format(
+                    datetime_str, current_year)
+
+                # Check if the datetime string is in the format "Month Year"
+            else:
+                # Convert it to "Hour:Minute:year Day Month Year"
+                datetime_str = "00:00:00 1 " + datetime_str
+
+            # Convert the datetime string to a datetime.datetime object
+            datetime_obj = datetime.strptime(
+                datetime_str, "%H:%M:%S %d %B %Y")
+
+        return datetime_obj
+
+    def days_between(date):
+        d1 = datetime.strptime(date, '%Y-%m-%d')
+        d2 = datetime.strptime(datetime.utcnow(), '%Y-%m-%d')
+        return abs((d2 - d1).days)
+
+    for i in range(0, 10):
+        params = {
+            'Group': '832',
+            'PageNum': i
+        }
+        groupURL = 'http://powdertoy.co.uk/Groups/Page/View.html'
+        page = session.get(groupURL, params=params)
+        soup = BeautifulSoup(page.text, "html5lib")
+
+        # Get all links in ul.TopiList#TopicList
+        # <ul id="TopicList" class="TopicList">
+
+        threadData = []
+        title = soup.find_all('a', {'class': 'Title'})
+        threads = [i["href"].split('&Thread=')[1] for i in title]
+        dates = [i.text for i in soup.find_all('span', {'class': 'Date'})]
+
+        for i in range(0, len(dates)):
+            threadData.append([threads[i], dates[i]])
+
+        for e in range(0, len(threadData)):
+            threadNum = threadData[e][0]
+            date = timestr_to_obj(threadData[e][1])
+
+            if days_between(date) >= 182 and not whitelist(threadNum):
+                # Lock thread
+                threadPost(lockMsg, threadNum, key)
+                threadModeration('lock', threadNum, key)
+            elif days_between(date) >= 200 and not whitelist(threadNum):
+                threadModeration('delete', threadNum, key)
+
+# vim:set shiftwidth=4 softtabstop=4 expandtab textwidth=79:

46c47139b1e877cd80d3e46a499937e73b4bcc31
wolfy1339 <wolfy1339@users.noreply.github.com>
2016-02-24 12:52:31 -0500

    __main__.py Fixes

--- src/__main__.py
+++ src/__main__.py
@@ -1,9 +1,10 @@
 import sys
 sys.path.insert(0, os.path.join(os.path.dirname(sys.modules[__name__].__file__), ".."))
 from main import TPT
-
+x = TPT()
 def main(arg):
-    TPT().cleanThreads(confirm=not "--automatic" in arg, delete=not "--lock" in arg)
+    """Wrapper function to pass arguments to the cleanThreads function"""
+    x.cleanThreads(confirm=not "--automatic" in arg, delete=not "--lock" in arg)
 if __name__ == '__main__':
     # Execute 'main' with all the command line arguments (excluding sys.argv[0], the program name).
     main(sys.argv[1:])
